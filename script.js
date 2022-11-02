14224f0822d50ed07fcf0d301a1fb873c0fea59c
BrownBearr <viswan.shiva@gmail.com>
2022-11-02 17:26:38 -0400

    not sure

---
+++ script.js
@@ -0,0 +1,12 @@
+let j;
+let i = 0;
+let responses = []
+let word = prompt('write a word');
+while(word != "stop" || "Stop"){
+    word = prompt('write a word');
+    responses [i] = word ;
+    i++ ;
+}
+console.log(responses);
+
+
