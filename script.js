ff283781f525047f4cf8d80afd970937e167c445
BrownBearr <viswan.shiva@gmail.com>
2022-11-07 16:24:45 -0500

    added lucky stuff 2 functions 3 events

--- script.js
+++ script.js
@@ -1,8 +1,44 @@
-document.getElementsByClassName("projectsIntro").addEventListener("mouseover", textChange);
+window.addEventListener('load', (event) => {
+    console.log("window loaded");
+    lucky();
+    luckyColor();
+});
 
+const text = document.getElementById("projectIntro");
+text.addEventListener('mouseover', () => {
+    document.getElementById("projectStory").style.fontSize = "40px";
+})
 
-function textChange();
+window.addEventListener('click', (event) => {
+    console.log("window loaded");
+    lucky();
+    luckyColor();
+});
+
+//clicks keep count -> change background of css style.background color 
+function clickCounter()
 {
-    const title = document.getElementById("heading");
+    var x = 0;
     
+}
+
+
+function lucky()
+{
+    var r = Math.floor(Math.random()*1000);
+    document.getElementById("number").innerText = `your lucky number is ${r}`;
+    console.log("lucky");
+}
+
+function luckyColor(){
+    const random_hex_color_code = () => {
+        let n = (Math.random() * 0xfffff * 1000000).toString(16);
+        const x = '#' + n.slice(0, 6);
+        document.getElementById("color").innerText = `your lucky color is ${x}`;
+        document.getElementById("colorBlock").style.backgroundColor = `${x}`;
+        console.log(`${x}`)
+      };
+      
+      console.log(random_hex_color_code())
+
 }
